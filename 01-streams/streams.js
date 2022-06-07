a33a8bee968df8d7486d71e280aa85cd1c88f5e0
Joaquin58 <91435597+Joaquin58@users.noreply.github.com>
2022-06-06 21:58:56 -0500

    agregado el concepto de streams

--- 01-streams/streams.js
+++ 01-streams/streams.js
@@ -19,6 +19,16 @@ for(let i=0; i<= 10; i++) {
 
 }
 
+/*Ahora ¿qué es lo que ocurre acá? Lo que ocurre con "stream" es que vamos a leer progresivamente un archivo.
+Eso significa que vamos a leer solo partes en cuanto se encuentren disponibles de este archivo y las vamos a 
+ir acumulando dentro del sistema. En el caso de la lecturafadicional sincrónica o asincrónica, vamos a esperar 
+hasta que el archivo se termine de leer y vamos a ejecutar el siguiente proceso. Así que, en el caso de este bucle 
+que se ejecuta diez veces, se va a ejecutar únicamente después de que ya leímos esos En este caso, en este bucle 
+vamos a ejecutar todos los comandos al mismo tiempo y se van a estar leyendo dentro del sistema. Los "streams" nos 
+van a permitir trabajar en diferentes hilos de manera asincrónica, y lo más importante es que no vamos a bloquear el 
+tiempo de ejecución de nuestras aplicaciones. Así que los "streams" nos van a ayudar en todo momento a mejorar el rendimiento 
+dramáticamente de nuestras aplicaciones en Node.*/
+
 
 
 console.timeEnd("tiempo de respuesta");
